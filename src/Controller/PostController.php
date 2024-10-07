c1cc7b34fdcc9a3e6dca5bb3a375b1077eb65423
raptor-cmd <josue091201@gmail.com>
2024-10-07 20:03:39 +0200

    Saving post in DB

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,16 +3,28 @@
 namespace App\Controller;
 
 use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
     #[Route('/post/crear', name: 'post_create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            $this->addFlash('success', "Publicación guardada con éxito");
+            return $this->redirectToRoute('post_create');
+        }
 
         return $this->render('post/create.html.twig', [
             'form' => $form->createView(),
