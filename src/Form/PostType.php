4f629db23192a3501fcb52f0c50c578853bb450b
raptor-cmd <josue091201@gmail.com>
2024-10-07 13:47:44 +0200

    CSRF protection available and customizing Post

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +15,24 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
-        ;
+            ->add('title', TextType::class, [
+                'label' => "Título de la publicación",
+                'help' => "Piensa en el SEO ¿Cómo buscaría en Google?"
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Contenido',
+                'attr' => ['rows' => 9, 'class' => 'bg-light']
+            ])
+            ->add('Enviar', SubmitType::class, [
+                'attr' => ['class' => 'btn-dark']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Post::class,
+            // 'csrf_protection' => false,
         ]);
     }
 }
