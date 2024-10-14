c03fbb02205b8f62c7c6f166b642b4d461d4da04
raptor-cmd <josue091201@gmail.com>
2024-10-14 15:39:56 +0200

    Mi versión de creación y edición de categorías

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category/crear', name: 'category_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CategoryType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Categoría creada con éxito');
+
+            return $this->redirectToRoute('category_create');
+        }
+
+        return $this->render('category/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/category/{id}/editar', name: 'category_edit', methods: ['GET', 'POST'])]
+    public function edit(Category $category, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Categoría actualizada con éxito');
+
+            return $this->redirectToRoute('category_edit', [
+                'id' => $category->getId(),
+            ]);
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
